be59e88242983ac80896ba64ea789a3c0793635d
Alessia Smeraglia <alessia.smeraglia@gmail.com>
2026-04-02 16:34:12 +0200

    Sostituite le classi con quelle Bootstrap

--- script.js
+++ script.js
@@ -1,27 +1,21 @@
 // Dichiaro le variabili per selezionare gli elementi del DOM
-const lamp = document.querySelector('img');
+const lamp = document.querySelector('.lamp');
 const btn = document.querySelector('#btn');
 const warningTitle = document.querySelector('#warning');
 
-// Aggiungo l'evento click al bottone
-btn.addEventListener("click", function () {
+// Aggiungo l'evento change allo switch
+btn.addEventListener("change", function () {
 
-    // Se la lampadina è accesa, la spengo
-    if (btn.innerText.includes('Spegni')) {
-        lamp.src = "./img/white_lamp.png";
-        lamp.alt = "Lampadina spenta";
-        btn.innerText = "Accendi";
-        btn.style.backgroundColor = "transparent";
-        btn.className = "accendi-spegni-btn";
-        warningTitle.classList.remove("active");
-    } else {
-        // Altrimenti accendo la lampadina
+    // Se lo switch è attivo, accendo la lampadina
+    if (btn.checked) {
         lamp.src = "./img/yellow_lamp.png";
         lamp.alt = "Lampadina accesa";
-        btn.innerText = "Spegni";
-        btn.style.backgroundColor = "red";
-        btn.className = "on";
         energySaving();
+    } else {
+        // Se lo switch non è attivo, spengo la lampadina
+        lamp.src = "./img/white_lamp.png";
+        lamp.alt = "Lampadina spenta";
+        warningTitle.classList.remove("active");
     }
 });
 
@@ -32,7 +26,7 @@ function energySaving() {
 
     // Se è giorno, mostro un avviso
     if (currentHours > 6 && currentHours < 17) {
-        alert("Attenzione! Non serve accendere la lampadina");
+        alert("Attenzione! Non serve accendere la lampadina, rilevata luce sufficiente.");
         warningTitle.classList.add("active");
     }
 }
